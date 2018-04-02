c1dfdd2e53e5d574e08a1d8f433ae75b85474432
lethargilistic <mikeoverby@outlook.com>
2018-04-01 20:14:48 -0700

    Make create-database work, update requirements.txt

--- database/create-database.py
+++ database/create-database.py
@@ -1,15 +1,8 @@
 #! /usr/bin/env python
 # Public domain; MZMcBride; 2012
 
-"""
-CREATE TABLE page (
-  page_title text,
-  page_text blob
-);
-CREATE UNIQUE INDEX page_title ON page (page_title);
-"""
-
 import sqlite3
+import os
 
 import wikitools
 
@@ -32,14 +25,28 @@ def get_category_members(category):
         category_members.append(member[u'title'])
     return category_members
 
+#Delete the database if it exists
+if os.path.exists(settings.dbname):
+    os.remove(settings.dbname)
+
+#Connect to database
 conn = sqlite3.connect(settings.dbname)
 cursor = conn.cursor()
 
+cursor.execute("""
+CREATE TABLE page (
+  page_title text,
+  page_text blob
+); """)
+cursor.execute('CREATE UNIQUE INDEX page_title ON page (page_title);')
+
+#Connect to wiki
 wiki = wikitools.Wiki(settings.apiurl)
 wiki.login(settings.username, settings.password)
 
+# Get the case data
 cases = get_category_members('United States Supreme Court cases')
-print(len(cases))
+print(len(cases)) #TODO: No way this is exactly 500. Investigate
 
 for case_title in cases:
     print(case_title)
