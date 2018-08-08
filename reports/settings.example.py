bdb878a96c608736bbd520be502f63affda647bf
lethargilistic <mikeoverby@outlook.com>
2018-08-08 02:52:02 -0700

    Add required fields to the example

--- reports/settings.example.py
+++ reports/settings.example.py
@@ -5,3 +5,7 @@ apiurl = 'https://en.wikipedia.org/w/api.php'
 
 # Database details
 dbname = '/home/user/wikiproject-scotus/wikiproject-scotus/database/scotus-cases.db'
+
+# Editing details
+rootpage = 'Wikipedia:WikiProject_U.S._Supreme_Court_cases/Reports/'
+editsumm = 'Updated page.'
